36c3e58fe537b8bd62316afa25693ea5e4e2190e
danielnachumdev <danielnachumdev@gmail.com>
2023-08-09 23:25:20 +0300

    small change

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from sys import argv
+from danielutils import get_directories
 
 
-def main():
-    albums_folder = argv[1]
-    print(albums_folder)
+def main() -> None:
+    base_folder = argv[1]
+    for album_folder in get_directories(base_folder):
+        print(album_folder)
 
 
 if __name__ == "__main__":
